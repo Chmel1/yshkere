261f7b7bb2e6a6f9fd617c4cb0f92ef193abf6ea
User <besvlat@bk.ru>
2024-09-26 11:45:42 +0300

    qwewqewqe

---
+++ WWP/WWP/Gridik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace WWP
+{ 
+        public class Gridik
+        {
+            public static void HideGrid(Grid selectedGrid, TextBlock tb, Grid parentKid)
+            {
+                foreach (var grid in parentKid.Children)
+                {
+                    if (grid.GetType() == typeof(Grid))
+                    {
+                        Grid grid1 = grid as Grid;
+                        grid1.Visibility = Visibility.Hidden;
+                    }
+                }
+                selectedGrid.Visibility = Visibility.Visible;
+                if (selectedGrid.Name == "first_grid")
+                {
+                    tb.Text = "";
+                }
+                else
+                {
+                    tb.Text = selectedGrid.Name.Replace('0', ' ');
+                }
+            }
+        }
+}
---
+++ WWP/WWP/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WWP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Gridik.HideGrid(I0want0have0runner, runner_text, MainGrid);
+
+        }
+
+        private void want_be_runner_Click(object sender, RoutedEventArgs e)
+        {
+            Gridik.HideGrid(I0want0have0runner, runner_text, MainGrid);
+        }
+    }
+}
